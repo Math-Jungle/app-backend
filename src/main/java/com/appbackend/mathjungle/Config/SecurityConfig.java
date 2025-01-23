86a807fce517d526b8c8e9dcfd78dd66417e8ca5
Nipuna Rajapaksa <rajapaksanipuna@gmail.com>
2025-01-23 16:36:17 +0530

    Spring security config

---
+++ src/main/java/com/appbackend/mathjungle/Config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.appbackend.mathjungle.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration//This is a configuration class to spring
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated()); //Making application to use the authentication
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//Make the session stateless
+        return http.build();//Return the object of Security Filter chain
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder((new BCryptPasswordEncoder(12)));
+        authProvider.setUserDetailsService(userDetailsService);
+        return authProvider;
+
+    }
+}
